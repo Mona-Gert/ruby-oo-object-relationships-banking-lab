0c375629642c3b3874d6effb36e51fddee30e852
monamohamed <mona.m.mohamed@gmail.com>
2020-12-22 21:27:22 -0500

    commit tree d341b7f4476ad9896f7d67b4e716c9f19186d761

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,30 @@
 class BankAccount
+    attr_reader :name
+    attr_accessor :balance, :status
+
+
+    def initialize(bankaccount)
+        #binding.pry
+     @bankaccount=bankaccount
+     @balance=1000
+     @status="open"
+     @name="Avi"
+    end
+
+    def deposit(money)
+    @balance +=1000
+    end
+
+    def display_balance 
+    "Your balance is $#{balance}."
+    end
+
+    def valid?
+    balance > 0 && status == "open"
+    end
+
+    def close_account
+        self.status="closed"
+    end
 
 end
